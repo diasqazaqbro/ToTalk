904d3ba5cd0cd5cb054052251e289bd28ba3d2fe
diasqazaqbro <adilovdiasdog2006@mail.ru>
2023-05-03 13:26:11 +0600

    feat: added redux-form and connect Login.jsx

--- src/componets/Login/Login.jsx
+++ src/componets/Login/Login.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+const LoginForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <Field component={"input"} name={"login"} />
+      <Field component={"input"} name={"password"} />
+      <Field component={"input"} type={"checkbox"} name={"rememberMe"} />
+      <button>Submit</button>
+    </form>
+  );
+};
+const LoginReduxForm = reduxForm({ form: "login" })(LoginForm);
 
 const Login = () => {
-    return (
-        <div>Login</div>
-    )
-}
-export default Login
+    const onSubmit = (formData ) => {
+        console.log(formData);
+    }
+  return <LoginReduxForm onSubmit={onSubmit} />;
+};
+export default Login;
