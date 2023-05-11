66c0849d1df02b5944b7733ee4c265ff99c3c1a4
diasqazaqbro <adilovdiasdog2006@mail.ru>
2023-05-11 20:41:43 +0600

    feat: successfully made login!!!

--- src/componets/Login/Login.jsx
+++ src/componets/Login/Login.jsx
@@ -2,24 +2,24 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 import { maxLengthCreator, requiredField } from "../../utils/validators";
 import { InputLogin } from "../UI/FormsControls/FormsControls";
-
-const maxLength10 = maxLengthCreator(10);
+import {connect} from 'react-redux'
+import {loginThunkCreator, logoutThunkCreator} from '../../redux/authReducer'
+const maxLength50 = maxLengthCreator(50);
 
 const LoginForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit}>
       <Field
-        validate={[requiredField, maxLength10]}
+        validate={[requiredField, maxLength50]}
         component={InputLogin}
-        name={"login"}
+        name={"email"}
       />
       <Field
-        validate={[requiredField, maxLength10]}
+        validate={[requiredField, maxLength50]}
         component={InputLogin}
         name={"password"}
       />
       <Field
-        validate={[requiredField, maxLength10]}
         component={"input"}
         type={"checkbox"}
         name={"rememberMe"}
@@ -30,10 +30,10 @@ const LoginForm = (props) => {
 };
 const LoginReduxForm = reduxForm({ form: "login" })(LoginForm);
 
-const Login = () => {
+const Login = (props) => {
   const onSubmit = (formData) => {
-    console.log(formData);
+    props.loginThunkCreator(formData.email, formData.password, formData.rememberMe)
   };
   return <LoginReduxForm onSubmit={onSubmit} />;
 };
-export default Login;
+export default connect(null, {loginThunkCreator, logoutThunkCreator})(Login);
--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,4 +1,4 @@
-import { getAuth } from "../api/api";
+import { getAuth, login, logout } from "../api/api";
 const SET_USER_DATA = "SET_USER_DATA";
 
 let initialState = {
@@ -13,26 +13,39 @@ const authReducer = (state = initialState, action) => {
     case SET_USER_DATA:
       return {
         ...state,
-        ...action.data,
+        ...action.payload,
         isAuth: true,
       };
     default:
       return state;
   }
 };
-export const setUserData = (userId, email, login) => ({
+export const setUserData = (userId, email, login, isAuth) => ({
   type: SET_USER_DATA,
-  data: { userId, email, login },
+  payload: { userId, email, login },
 });
 
 export const getAuthThunkCreator = () => (dispatch) => {
   getAuth().then((response) => {
     if (response.data.resultCode === 0) {
       let { id, email, login } = response.data.data;
-      dispatch(setUserData(id, email, login));
+      dispatch(setUserData(id, email, login, true));
     }
   });
 };
 
-export const loginThunkCreator = (email, password, rememberMe) => (dispatch) => {};
+export const loginThunkCreator = (email, password, rememberMe) => (dispatch) => {
+  login(email, password, rememberMe).then(response => {
+    if (response.data.resultCode === 0) {
+      dispatch(getAuthThunkCreator())
+    }
+  })
+};
+export const logoutThunkCreator = () => (dispatch) => {
+  logout().then(response => {
+    if (response.data.resultCode === 0) {
+      dispatch(setUserData(null, null, null, false))
+    }
+  })
+};
 export default authReducer;
