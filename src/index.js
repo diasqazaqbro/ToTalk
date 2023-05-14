7ea22cbcb5875cd65d871b2eb3857a6c523328d2
diasqazaqbro <adilovdiasdog2006@mail.ru>
2023-05-15 02:25:09 +0600

    refactor: change browserouter on hashrouter for gh-pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import store from './redux/redux-store'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import store from "./redux/redux-store";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.render
-   (<BrowserRouter>
-      <Provider store={store}>
-         <App />
-      </Provider>
-   </BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>,
+  document.getElementById("root")
+);
 
 reportWebVitals();
